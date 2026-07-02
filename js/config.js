dd3b08c6281c20448cda998021ac5a179e72a8ce
Alexander Mills <trainyouragent@gmail.com>
2026-07-02 00:02:08 -0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@
 // ==========================================================
 window.PN_CONFIG = {
   SUPABASE_URL: "https://akbtbwahwmbipeorzjar.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_xryHIEI9usV056lw3pj62Q_MwerzDxR",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrYnRid2Fod21iaXBlb3J6amFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMDAyNDMsImV4cCI6MjA5NzU3NjI0M30.U2miHo-i6ZZIziVltHOnP69OfJ8R1bT1c4W19GS4MFM",
   HERO_VIDEO: "https://d8j0ntlcm91z4.cloudfront.net/user_342M7OMJEmtQi5ZXBKPVqJZUjCn/hf_20260702_033523_e7375190-6ac1-4f72-8a10-991c65721bea.mp4"
 };
